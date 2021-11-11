667ba829ab2a9534678f68cd78e8f74adffb1a39
ley-v <83968941+ley-v@users.noreply.github.com>
2021-11-11 00:21:07 +0000

    aula 02 - criando layout de cadastro do subscriber

---
+++ app/src/main/java/com/example/a02crudcomroom/ui/subscriber/SubscriberFragment.kt
@@ -0,0 +1,24 @@
+package com.example.a02crudcomroom.ui.subscriber
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a02crudcomroom.R
+
+class SubscriberFragment : Fragment() {
+
+
+
+    private lateinit var viewModel: SubscriberViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.subscriber_fragment, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/a02crudcomroom/ui/subscriber/SubscriberViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.a02crudcomroom.ui.subscriber
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
