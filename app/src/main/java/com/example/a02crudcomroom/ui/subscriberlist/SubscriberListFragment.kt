243ee4eec8fee4478351b678be52389861197247
ley-v <83968941+ley-v@users.noreply.github.com>
2021-11-12 21:07:19 +0000

    aula 06 - criando o layout de listagem de subscriber

---
+++ app/src/main/java/com/example/a02crudcomroom/ui/subscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,15 @@
+package com.example.a02crudcomroom.ui.subscriberlist
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a02crudcomroom.R
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+
+
+}
---
+++ app/src/main/java/com/example/a02crudcomroom/ui/subscriberlist/SubscriberListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.a02crudcomroom.ui.subscriberlist
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
