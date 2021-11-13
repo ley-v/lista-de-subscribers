302ddaa3a4029e1da0360318402e449e6960b4d2
ley-v <83968941+ley-v@users.noreply.github.com>
2021-11-13 11:45:09 +0000

    aula 09 - navegando entre a tela de listagem e a tela de cadastro de subscribers

--- app/src/main/java/com/example/a02crudcomroom/ui/subscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/com/example/a02crudcomroom/ui/subscriberlist/SubscriberListFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.a02crudcomroom.R
 import com.example.a02crudcomroom.data.db.AppDatabase
 import com.example.a02crudcomroom.repository.DatabaseDataSource
@@ -28,7 +29,10 @@ class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //quando nossa view estiver disponível com todos os recursos e componentes, chamaremos as funções abaixo
+        //para tornar o código mais legível é bom deixar o onViewCreated apenas com funções que tenham nomes sugestivos
         observeViewModelEvents()
+        configureViewListeners()
     }
 
     private fun observeViewModelEvents() {
@@ -41,5 +45,17 @@ class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
                 adapter = subscriberListAdapter
             }
         })
+    }
+
+    private fun configureViewListeners(){
+        fabAddSubscriber.setOnClickListener {
+            //Aqui chamamos a função de navegação do navigation controller
+            //A view consegue encontrar o findNavController pq está dentro de uma activity que tem configurado o fragment container
+            //Desde que as duas telas estejam dentro do mesmo gráfico de navegação podemos utilizar o 'id' dentro do método
+            // navigate, podemos conferir apertando ctrl + nome do fragment
+            //ps: a forma recomendada(que será visto depois) é fazer a ligação de uma tela para a outra e navegar usando o id dessa
+            // 'ação'(<action>)
+            findNavController().navigate(R.id.subscriberFragment)
+        }
     }
 }
